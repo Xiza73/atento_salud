127937f529ba0375e52604c2cc4a887b5bcc4adf
Marco <marco.fura@unmsm.edu.pe>
2022-01-03 15:32:28 -0500

    import added

--- src/pages/admin/asegurados/index.jsx
+++ src/pages/admin/asegurados/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Adminside from "../../../components/AdminSide";
 import LayoutAdmin from "../../../components/layout/LayoutAdmin";
 
